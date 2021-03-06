d49e83106634f30d9ea5e9588df89d0eb389d5a4
shinixsensei <crezoxplays@gmail.com>
2021-03-06 16:01:47 +0100

    more data

--- src/main/java/util/Data.java
+++ src/main/java/util/Data.java
@@ -11,10 +11,6 @@ public class Data {
         if(!Local.propExist(obj.question)) {
             Local.addKey(Jsoup.parse(obj.question).text(), Jsoup.parse(obj.correctAnswer).text());
             System.out.println("[reTrivia Local] Saved '" + Jsoup.parse(obj.question).text() + "' with '" + Jsoup.parse(obj.correctAnswer).text() + "'");
-        }else {
-            Local.addKey(Jsoup.parse(obj.question).text(), Jsoup.parse(Local.getValue(obj.question)).text());
-            Local.deleteKey(obj.question);
-            System.out.println("[reTrivia Local] Relocated '" + Jsoup.parse(obj.question).text() + "'");
         }
     }
 
